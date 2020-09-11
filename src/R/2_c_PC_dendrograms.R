1a4f1aea3e9fc9dfd8cefdf4406bf7489fffefd7
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-09-11 06:17:00 -0400

    Correct typos

--- src/R/2_c_PC_dendrograms.R
+++ src/R/2_c_PC_dendrograms.R
@@ -2,22 +2,22 @@ library(Seurat)
 library(tidyverse)
 
 # Data not provided in the repository.
-obj <- readRDS('clean_neurons.rds')
+obj <- readRDS("../meta_cluster_analysis/clean_neuro_final.rds")
 
-centroids <- as_tibble(Embeddings(obj, reduction = 'integrated_PCA')) %>%
-  add_column('cluster' = as.characer(
-    obj[['final_cluster_assignment']][[1]]
+centroids <- as_tibble(Embeddings(obj, reduction = "integrated_PCA")) %>%
+  add_column("cluster" = as.character(
+    obj[["final_cluster_assignment"]][[1]]
   )) %>%
-  pivot_longer(-cluster, names_to = 'PC', values_to = 'coord') %>%
-  mutate_at('PC', str_remove_all, pattern = '\\D') %>%
-  mutate_at('PC', as.numeric) %>%
+  pivot_longer(-cluster, names_to = "PC", values_to = "coord") %>%
+  mutate_at("PC", str_remove_all, pattern = "\\D") %>%
+  mutate_at("PC", as.numeric) %>%
   group_by(cluster, PC) %>%
-  summarise('avg.pc' = mean(coord)) %>%
+  summarise("avg.pc" = mean(coord)) %>%
   ungroup() %>%
   pivot_wider(names_from = cluster, values_from = avg.pc) %>%
   select(-PC) %>%
   slice(1:50)
 
 distance <- dist(t(centroids))
-hc <- hclust(distance, method = 'complete')
+hc <- hclust(distance, method = "complete")
 plot(hc, hang = -1)
